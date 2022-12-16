f561cc57d52b86ed63dfa4fe23afd2029b71cf1e
“Alexbarile” <alexbarile93fg@yahoo.it>
2022-12-16 20:09:37 +0100

    BONUS aggiunta immagine a schermo con data

--- js/script.js
+++ js/script.js
@@ -6,7 +6,19 @@ createApp({
     data(){
         return{
             name: '',
-            surname: ''
+            surname: '',
+            name_surname: '',
+
+            // usata v-bind
+            red: 'text-danger',
+
+            // BONUS richiamo foto 
+            image: 'https://picsum.photos/200/300'
+        }
+    },
+    methods:{
+        greeting: function(){
+            this.name_surname = `${this.name} ${this.surname}` 
         }
     }
 }).mount('#container')
