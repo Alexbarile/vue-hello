ed9ce83f7e8459b29ec408ec8651f5664c2c8a84
Alexbarile <alexbarile93fg@yahoo.it>
2022-12-17 20:55:46 +0100

    ottimizzazione codice e aggiunto il methods in js

--- js/script.js
+++ js/script.js
@@ -11,14 +11,23 @@ createApp({
 
             // usata v-bind
             red: 'text-danger',
+            blue: 'text-blue',
+            visibility: 'd-none',
 
             // BONUS richiamo foto 
-            image: 'https://picsum.photos/200/300'
+            image: 'https://picsum.photos/200/300',
         }
     },
     methods:{
-        greeting: function(){
-            this.name_surname = `${this.name} ${this.surname}` 
+        greetings: function(){
+            this.name_surname = `${this.name} ${this.surname}`;
+            if(this.blue == 'text-blue'){
+                this.blue = 'text-green';
+            }
+            else{
+                this.blue = 'text-blue';
+            }
+            this.visibility = 'display-block';
         }
     }
 }).mount('#container')
